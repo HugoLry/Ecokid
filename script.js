b7808d7e4cefdb8825f1eae685bc9108aaa8c318
Obicho <obicho@users.noreply.github.com>
2020-11-08 15:11:29 +0100

    transition et 2nde page

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-$(function() {
-    $("a").click(function() {
+$(function () {
+    $("a").click(function () {
         var link = $(this).attr("href");
+        $("input, #accueil, h1, h2").css("transform", "translateY(100vh)");
+        $("img.transition-out").css("top", "0vh");
         return lancerPage(link);
     });
- 
+
     function lancerPage(a) {
-        redirect(a);zeqetsef
+        redirect(a);
         return false;
     }
- 
+
     function redirect(lnk) {
-        setTimeout(function() {
+        setTimeout(function () {
             window.location.href = lnk;
         }, 2000);
     }
