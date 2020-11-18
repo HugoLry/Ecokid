5731bef4ed4ca7b8e6b0ee4bb0496a425aabe3ad
Obicho <obicho@users.noreply.github.com>
2020-11-18 12:03:12 +0100

    Transition fond / Ajout images poubelles

--- pages/jeu/script.js
+++ pages/jeu/script.js
@@ -1,29 +1,5 @@
-$(function () {
-    
-    $("a").click(function () {
-        var link = $(this).attr("href");
-        $("input, #accueil, h1, h2").css({
-            "transition": "all 1s ease",
-            "transform": "translateY(100vh)"
-        });
-        $("img.transition-out").css("top", "0vh");
-        return lancerPage(link);
-    });
-
-    function lancerPage(a) {
-        redirect(a);
-        return false;
-    }
-
-    function redirect(lnk) {
-        setTimeout(function () {
-            window.location.href = lnk;
-        }, 2000);
-    }
-
-    $('input').click(function () {
-        alert("test");
-        $('.dechets').css({"transition":"all 1s ease","transform":"scale(0)"});
-        
-    });
+$('input').click(function () {
+    $('.dechets').css("transform","scale(0)");
+    $('.fond').css('opacity','0')
+   
 });
